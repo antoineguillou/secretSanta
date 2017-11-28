cc6541209d39f66b55ee6c1111135c51aa4b7ea2
Antoine Guillou <contact@antoineguillou.fr>
2017-11-28 14:16:11 +0100

    clean repository

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
-/* Author:
+(function($){
+	function reloadWindow(){
+		$.ajax({
+		   url: 'list.php',
+		   dataType: 'html',
+		   success: function(result){
+		   		$('.windows').empty().append(result);
+		   },
+		   alert: function(result){
+		   		console.log(result);
+		   }
+		});
+	}
+	reloadWindow();
 
-*/
-
-
-
-
-
-
-
+  $('a').on('mouseup touch', function(e){
+    reloadWindow();
+  });
+})(jQuery);
